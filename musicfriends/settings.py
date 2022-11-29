b641d0c3621b5bec554d6e79fb9ab4cebd25630f
daniel-layton-wright <daniel.layton.wright@gmail.com>
2022-11-28 17:51:08 -0800

    Turn off TZ (try fix error)

--- musicfriends/settings.py
+++ musicfriends/settings.py
@@ -115,7 +115,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -131,8 +131,8 @@ LOGIN_URL = '/login'
 
 SPOTIFY_CLIENT_ID = 'ef193af183eb4605bb960861f62d7fdd'
 SPOTIFY_CLIENT_SECRET = 'fadae9ff124343ea928b12f0e16a4308'
-SPOTIFY_REDIRECT_URI = 'http://musicfriends.herokuapp.com/spotifycallback'
-SPOTIFY_SCOPE = 'user-read-private user-read-email user-library-read user-follow-read'
+SPOTIFY_REDIRECT_URI = 'http://localhost:8000/spotifycallback'
+SPOTIFY_SCOPE = 'user-read-private user-read-email user-library-read user-follow-read playlist-read-collaborative playlist-read-private'
 
 django_heroku.settings(locals())
 
